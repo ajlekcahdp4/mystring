0e86b6ac97c820138ce82de9da53084876f2dc2f
ajlekcahdp4 <alex.rom23@mail.ru>
2022-07-20 12:52:09 +0300

    add const keyword in class functions, change operator==

--- mystring.hpp
+++ mystring.hpp
@@ -21,18 +21,14 @@ class mystring {
         std::strncpy (data_, data, size_);
     }
 
-    virtual ~mystring ()
-    {
-        delete[] data_;
-        std::cout << "dstr";
-    }
+    virtual ~mystring () { delete[] data_; }
 
-    size_t size () { return size_; }
+    size_t size () const { return size_; }
 
-    char *c_str () { return data_; }
+    char *c_str () const { return data_; }
 };
 
-bool operator== (mystring str1, mystring str2)
+long operator== (const mystring &str1, const mystring &str2)
 {
     int diff = str1.size () - str2.size ();
     return diff == 0 ? std::strncmp (str1.c_str (), str2.c_str (), str1.size ()) : diff;
